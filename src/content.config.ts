45ff6e85127f37152bff3f9fe58c89f13e064ad0
Anetos882 <74851720+Anetos882@users.noreply.github.com>
2026-05-14 11:50:19 +0500

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,25 +1,24 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
 
 const games = defineCollection({
-  loader: glob({
-    base: "./src/content/games",
-    pattern: "**/*.md",
-  }),
+  type: "content",
   schema: z.object({
-    title: z.string(),
-    bgg_id: z.number(),
-    date_added: z.coerce.date().optional(),
-    my_rating: z.number().optional(),
-    plays: z.number().optional(),
-    year: z.number().optional(),
-    bgg_rank: z.number().optional(),
-    bayes_rating: z.number().optional(),
-    average_rating: z.number().optional(),
-    users_rated: z.number().optional(),
-    localization: z.string().optional(),
     bgg_url: z.string().optional(),
-    image: z.string().optional(),
+    bgg_id: z.number().optional(),
+
+    localization: z.string().optional(),
+    date_added: z.coerce.date().optional(),
+    plays: z.number().optional(),
+    my_rating: z.number().optional(),
+
+    image: z.string().nullable().optional(),
+    title: z.string().nullable().optional(),
+
+    year: z.coerce.number().nullable().optional(),
+    bgg_rank: z.coerce.number().nullable().optional(),
+    bayes_rating: z.coerce.number().nullable().optional(),
+    average_rating: z.coerce.number().nullable().optional(),
+    users_rated: z.coerce.number().nullable().optional(),
   }),
 });
 
